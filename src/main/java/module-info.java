04a15cec76911285b96ef788a6d79de9cd4b14e8
seham <seham@DESKTOP-OH4KNPC>
2023-10-21 22:32:24 -0400

    First Commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,9 @@
+/* doesn't work with source level 1.8:
+module com.mycompany.hellogithub {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    opens com.mycompany.hellogithub to javafx.fxml;
+    exports com.mycompany.hellogithub;
+}
+*/
